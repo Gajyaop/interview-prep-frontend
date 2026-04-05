b1aa1fc50406b879c2de0446913fdf5595502bca
harsh <hp200091@gmail.com>
2026-04-05 10:54:42 +0530

    update base url to the one of railway

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'interview-prep-backend-production-8f9c.up.railway.app',
 });
 
 api.interceptors.request.use((config) => {
